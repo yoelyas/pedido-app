78a310875e22865df211f0061a829f9677a2ac5f
yoelyas <44931169+yoelyas@users.noreply.github.com>
2022-01-12 12:08:27 -0300

    a

--- lib/widgets/barra_de_busqueda.dart
+++ lib/widgets/barra_de_busqueda.dart
@@ -24,7 +24,6 @@ class _MyCustomFormState extends State<MySearchForm> {
     // Clean up the controller when the widget is disposed.
     myController.dispose();
     super.dispose();
-    print("a");
   }
 
   @override
